309bdfb50f60fa2096f4bc14ee8b7fc3bbd0d44c
akrhakki <akarhakki4@gmail.com>
2022-09-20 10:05:05 +0300

    DolarFiyatinaGöreOkIsaretiKonuldu

---
+++ intro/src/intro/main.java
@@ -0,0 +1,24 @@
+package intro;
+
+public class main {
+
+	public static void main(String[] args) {
+		double dolarBugun=18.15;
+		double dolarDun=18.15;
+		String okYonu="";
+		if (dolarBugun<dolarDun) {
+			okYonu="down.svg";
+			System.out.println(okYonu);
+		}
+		else if (dolarBugun>dolarDun) {
+			okYonu="up.svg";
+			System.out.println(okYonu);
+		}
+		else {
+			okYonu="equal.svg";
+			System.out.println(okYonu);
+		}
+	}
+
+}
+ 
